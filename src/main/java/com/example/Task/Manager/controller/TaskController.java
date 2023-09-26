ab26e143e276e65501e8ce5e699f36f53344fcd6
Nidhi-Sanklecha <nidhisanklecha8@gmail.com>
2023-09-26 15:43:50 +0530

    Added Swagger documentation

--- src/main/java/com/example/Task/Manager/controller/TaskController.java
+++ src/main/java/com/example/Task/Manager/controller/TaskController.java
@@ -2,6 +2,9 @@ package com.example.Task.Manager.controller;
 
 import com.example.Task.Manager.dao.TaskRepo;
 import com.example.Task.Manager.model.Task;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +12,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@OpenAPIDefinition(
+        info = @Info(
+                title = "APIs",
+                version = "1.0.0",
+                description = "APIS for performing CRUD operations on Task Manager",
+                contact = @Contact(
+                        name = "Nidhi",
+                        email = "int.Nidhi.Sanklecha@xpressbees.com"
+        )
+        )
+)
 public class TaskController {
     @Autowired
     private TaskRepo repo;
